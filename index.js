a7aa5db9ef3b1e5ddbb72bbe02a69548bc794c18
Gerald <mcamanor@gmail.com>
2020-07-04 01:51:41 +0000

    Added styling to body tag

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const path = require('path')
 
 const server = express()
+
+server.use(express.static(path.join(__dirname, 'public')))
 
 server.listen(3000, ()=>console.log('Server is fired up'))
